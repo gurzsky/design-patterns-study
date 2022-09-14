3717ff6a385ac0b37ed7e367eb1b65e334076ae6
gurzsky <gurski.ce@gmail.com>
2022-09-14 09:09:36 -0300

    design patter proxy

--- src/com/designpatterns/structural/proxy/Main.java
+++ src/com/designpatterns/structural/proxy/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Usuario pessoaFisica = new Usuario("Joao", "123456789", 25);
         System.out.println(pessoaFisica.getNome());
+
+        pessoaFisica.validaNome();
     }
 }
